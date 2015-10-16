1149fed0b3720c9e32992e55f8da40bf226189d9
andreasTest <nikolakopoulosand@gmail.com>
2015-10-16 16:38:15 +0200

    jean

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,18 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 System.out.println("branche deux");
-		 System.out.println("modif");System.out.println("modif");
-		 System.out.println("modif");
+		
+		
+			
+			
+		}
+		
+	
+
+	public void factorielle (int n){
+	
+		System.out.println("hello");
 	}
+
 
 }
