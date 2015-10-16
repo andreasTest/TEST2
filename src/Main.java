60a38b40313c3335174ab88f30ad681e1423c68d
toine16 <antoine.brousmiche@gmail.com>
2015-10-16 16:36:21 +0200

    ok

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,26 @@ public class Main {
 		
 
 	}
-
+	/**
+	 * 
+	 * @param i
+	 * @return
+	 */
+	public int factorielle(int i)
+	{
+		int y = i;
+		for(i;i>0;i++)
+		{
+			y *= i;
+		}
+		
+	return y;
+	//
+	}
+	
+	public int rienAFaire()
+	{
+		return 10;
+	}
 }
+a
